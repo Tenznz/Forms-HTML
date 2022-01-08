333e1db23e1b46fa1f602f6adcf1d11598c3e5f6
tenznz <dhugkar@gmail.com>
2022-01-08 00:49:06 -0800

    Added Regex for password rule1
    Rule1: Minimum 8 character

--- js/Forms.js
+++ js/Forms.js
@@ -12,28 +12,36 @@ text.addEventListener('input', function () {
 
 const email = document.querySelector('#email');
 const emailError = document.querySelector('.email-error');
-email.addEventListener('input',function(){
+email.addEventListener('input', function () {
     let emailRegex = RegExp('^[0-9a-zA-Z+-._]+@[0-9a-zA-Z]+.[a-zA-Z]{2,3}.([a-zA-z]{2,3})');
-    if(emailRegex.test(email.value)){
-        emailError.textContent="";
-    }else{
-        emailError.textContent="Email is InValid";
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Email is InValid";
     }
 });
 
 const phone = document.querySelector('#tel');
-const errorTel = document.querySelector('.tel-error');
-phone.addEventListener('input',function(){
+const telError = document.querySelector('.tel-error');
+phone.addEventListener('input', function () {
     let phoneRegex = RegExp('^[+][0-9]{1,2}?[\\s,-][7-9]{1}[0-9]{9}$');
-    if(phoneRegex.test(phone.value)){
-        errorTel
-    .textContent="";
-    }else{
-        errorTel
-    .textContent="Phone number is incorrect";
+    if (phoneRegex.test(phone.value)) {
+        telError.textContent = "";
+    } else {
+        telError.textContent = "Phone number is incorrect";
     }
 });
 
+const password = document.querySelector("#pwd");
+const pwdError = document.querySelector(".pwd-error");
+password.addEventListener('input',function(){
+    let passRegex = RegExp("^[A-Za-z]{8,}$");
+    if(passRegex.test(password.value)){
+        pwdError.textContent="";
+    }else{
+        pwdError.textContent="Password is incorrect";
+    }
+});
 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
